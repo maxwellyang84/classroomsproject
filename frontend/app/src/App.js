0d1a195db77b7eaa7e6886c3e3a042bdd0eac064
Maxwell Yang <maxwellyang84@gmail.com>
2020-05-30 01:59:47 -0700

    integrated apollo client into frontend for react

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
+import {gql} from 'apollo-boost';
+import {useQuery} from '@apollo/react-hooks';
+
+const ACTIVITIES = gql`
+  {
+    getActivities{
+      Activity_Type
+    }
+  }
+`;
 
 function App() {
-  return (
-    <div className="App">
- 
-    </div>
-  );
+  const {loading, error, data} = useQuery(ACTIVITIES);
+  if(loading) return <p>Loading...</p>;
+  if(error) return <p> Error :(</p>
+    data.getActivities.map(({Activity_Type})=>{
+      console.log(Activity_Type)
+    })
+    return  data.getActivities.map(({Activity_Type})=>(
+      <div key={Activity_Type}>
+        <p>{Activity_Type ? Activity_Type: "Nothing"}</p>
+      </div>
+    ))
+    
+
+
+  // return (
+  //   <div className="App">
+  //     <p>MM</p>
+  //   </div>
+  // );
 }
 
 export default App;
--- frontend/app/src/index.js
+++ frontend/app/src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient, {gql} from 'apollo-boost';
+import {ApolloProvider} from '@apollo/react-hooks';
+
+const client = new ApolloClient({
+  uri: "http://localhost:8000"
+});
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+       <App />
+    </ApolloProvider>
+ 
   </React.StrictMode>,
   document.getElementById('root')
 );
