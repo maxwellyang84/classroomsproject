07beba4587b2be532e6874f8aae647350d422c1a
Maxwell Yang <maxwellyang84@gmail.com>
2020-06-16 02:54:06 -0700

    working on hover effect in search bar

--- frontend/app/src/components/SearchTable.js
+++ frontend/app/src/components/SearchTable.js
@@ -6,6 +6,8 @@ import SearchTableForm from './SearchTableForm';
 import Navbar from './Navbar';
 import './SearchTable.css'
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
 
 function SearchTable(){
     const [date, setDate] = useState('1999-10-01')
@@ -30,14 +32,23 @@ function SearchTable(){
             />
         </div> */}
         <div className="search-form">
+            <div className="building-section">
             <p className="building-header">Buildings</p>
-            <p className="building-subheader">Select Buildings to Include</p>
-            <div className="line-1"/>
-            <p className="date-header">Date</p>
+                <p className="building-subheader">Select Buildings to Include</p>
+                <div className="line-1"/>
+            </div>
+                
             
-            <div className="line-2"/>
-            <p className="starttime-header">Start Time</p>
-            <div className="line-3"/>
+            <div className="date-section" id="date-container">
+                <p className="date-header">Date</p>
+                <Form.Control type="date" container="date-container" className="date"/>
+                <div className="line-2"/>
+            </div>
+            <div>
+                <p className="starttime-header">Start Time</p>
+                <div className="line-3"/>
+            </div>
+            
             <p className="endtime-header">End Time</p>
             <Button variant="primary" type="submit">Submit</Button>
         </div>
